b54a1b466904dbd65fddb154740794155c465a10
christiecamp <christiecamphoto@gmail.com>
2024-01-03 01:14:57 -0500

    created skeletal files for user and thought controllers

---
+++ controllers/thought.js
@@ -0,0 +1,15 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+
+//all thoughts
+//thoughts by id
+//create thought
+//update thought
+//delete thought
+//add reaction
+//delete reaction
+
+};
+
+module.exports = thoughtController;
--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,15 @@
 const { User, Thought } = require('../models');
 
+const userController = {
 
+//all users
+//user by id
+//create user
+//update user
+//delete user
+//add friend
+//delete friend
 
-
-
+};
 
 module.exports = userController;
