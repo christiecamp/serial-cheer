9158e049fbe7ebf721a4b5a066b4359daea6b946
christiecamp <christiecamphoto@gmail.com>
2024-01-03 12:32:10 -0500

    working through user controller - created get user & get user by id

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,14 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 // const { connect, connection } = require('mongoose');
 
+//wrap mongoose around mongodb local connection
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/serialsocial', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
+//log all executed collection methods & their arguments to console
+mongoose.set('debug', true);
+
+//export connection
 module.exports = mongoose.connection;
--- controllers/user.js
+++ controllers/user.js
@@ -2,8 +2,34 @@ const { User, Thought } = require('../models');
 
 const userController = {
 
-//all users
-//user by id
+    //GET all users
+    async getUsers(req, res) {
+        try {
+            const users = await User.find()
+                // .populate({ path: 'thoughts', select: '-__v' })
+                .populate({ path: 'friends', select: '-__v' });
+            return res.status(200).json(users);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+
+    //GET user by id
+    async getUserById(req, res) {
+        try {
+            const user = await User.findById({ _id: req.params.id })
+                .populate({ path: 'thoughts', select: '-__v' })
+                .populate({ path: 'friends', select: '-__v' });
+            if (!user) {
+                return res.status(404).json({ message: 'id not found' })
+            }
+            return res.status(200).json(user);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
 //create user
 //update user
 //delete user
