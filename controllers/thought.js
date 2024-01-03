6568b3557266f619617d84152e0d68be50df959f
christiecamp <christiecamphoto@gmail.com>
2024-01-03 14:43:09 -0500

    working through thought controller

--- controllers/thought.js
+++ controllers/thought.js
@@ -2,14 +2,23 @@ const { Thought, User } = require('../models');
 
 const thoughtController = {
 
-//all thoughts
-//thoughts by id
-//create thought
-//update thought
-//delete thought
-//add reaction
-//delete reaction
+    //all thoughts
+    async getThoughts(req, res) {
+        try{
+            const thoughts = await Thought.find();
+            return res.status(200).json(thoughts);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
 
+    //thoughts by id
+    //create thought
+    //update thought
+    //delete thought
+    //add reaction
+    //delete reaction
 };
 
 module.exports = thoughtController;
