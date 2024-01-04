c6e32d7049c0d3d4e865ec76ec744933ed3cf6f8
christiecamp <christiecamphoto@gmail.com>
2024-01-04 17:06:49 -0500

    working through seed data

---
+++ utils/data.js
@@ -0,0 +1,13 @@
+//users
+const users = [];
+//username, email, thought
+
+//thoughts
+const thoughts = [];
+//thoughtText - with username
+
+//reactions
+const reactions = [];
+//reactionBody - with username
+
+module.exports = { users, thoughts, reactions }
---
+++ utils/seed.js
@@ -0,0 +1,29 @@
+const connection = require('../config/connection');
+const { User, Thought } = require('../models');
+const { users, thoughts } = require('./data');
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+    //change to ascii during polish
+    console.log('connected');
+    //delete collections if they exist
+    await User.deleteMany({});
+    await Thought.deleteMany({});
+
+    //seeded data exists within empty arrays
+    const users = [];
+    const thoughts = [];
+
+    //add objects - user
+
+    //add objects - thoughs
+
+//loop through & display
+console.table(users);
+console.table(thoughts);
+console.info('Seeding complete! 🌱');
+process.exit(0);
+});
+
+
